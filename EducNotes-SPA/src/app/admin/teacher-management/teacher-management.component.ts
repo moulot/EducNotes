68762240854271040fc44233f166bbd9919115bb
MohKabore <mhkabore@gmail.com>
2019-11-02 17:04:09 +0000

    step_201911010000

--- EducNotes-SPA/src/app/admin/teacher-management/teacher-management.component.ts
+++ EducNotes-SPA/src/app/admin/teacher-management/teacher-management.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup,  FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { AlertifyService } from 'src/app/_services/alertify.service';
 import { Course } from 'src/app/_models/course';
 import { NzMessageService } from 'ng-zorro-antd';
@@ -15,7 +15,7 @@ import * as XLSX from 'xlsx';
   selector: 'app-teacher-management',
   templateUrl: './teacher-management.component.html',
   styleUrls: ['./teacher-management.component.css'],
-  animations :  [SharedAnimations]
+  animations: [SharedAnimations]
 })
 export class TeacherManagementComponent implements OnInit {
   teachersCourses: any[];
@@ -30,24 +30,24 @@ export class TeacherManagementComponent implements OnInit {
   selectedTeacher: any;
   teacher: any;
   editModel: any;
- editionMode = 'add';
- show = 'all';
- drawerTitle: string;
- userId: number;
- visible: boolean;
- affectaionVisible: boolean;
- submitText = 'enregistrer';
- dateOfBirth: string;
+  editionMode = 'add';
+  show = 'all';
+  drawerTitle: string;
+  userId: number;
+  visible: boolean;
+  affectaionVisible: boolean;
+  submitText = 'enregistrer';
+  dateOfBirth: string;
 
   constructor(private adminService: AdminService, private fb: FormBuilder,
-     private classService: ClassService, private router: Router, private alertify: AlertifyService,
-     private route: ActivatedRoute,  private nzMessageService: NzMessageService) { }
+    private classService: ClassService, private router: Router, private alertify: AlertifyService,
+    private route: ActivatedRoute, private nzMessageService: NzMessageService) { }
 
   ngOnInit() {
 
     this.route.data.subscribe(data => {
       this.teachersCourses = data.teachers;
-   });
+    });
   }
 
   getTeachers() {
@@ -65,8 +65,8 @@ export class TeacherManagementComponent implements OnInit {
     let classIds = [];
     for (let i = 0; i < params.courseClasses.length; i++) {
       const cours = params.courseClasses[i].course;
-      courses =  [...courses, cours];
-      const classes =  params.courseClasses[i].classes;
+      courses = [...courses, cours];
+      const classes = params.courseClasses[i].classes;
       for (let j = 0; j < classes.length; j++) {
         const cl = classes[j];
         classIds = [...classIds, cl];
@@ -75,17 +75,17 @@ export class TeacherManagementComponent implements OnInit {
     }
     params.classes = classIds;
     params.courses = courses;
-   this.teacher = params;
+    this.teacher = params;
 
-   this.show = 'affect';
+    this.show = 'affect';
   }
 
 
   saveImport() {
     this.adminService.importTeachersFile(this.exportedTeachers).subscribe(() => {
-     this.alertify.success('enregistrement terminé...');
-     this.getTeachers();
-     this.show = 'all';
+      this.alertify.success('enregistrement terminé...');
+      this.getTeachers();
+      this.show = 'all';
     }, error => {
       this.alertify.error(error);
     });
@@ -93,70 +93,70 @@ export class TeacherManagementComponent implements OnInit {
   }
 
 
-    onFileChange(ev) {
-      let workBook = null;
-      let jsonData = null;
-      const reader = new FileReader();
-      const file = ev.target.files[0];
-      reader.onload = (event) => {
-        const data = reader.result;
-        workBook = XLSX.read(data, { type: 'binary' });
-        jsonData = workBook.SheetNames.reduce((initial, name) => {
-          const sheet = workBook.Sheets[name];
-          initial[name] = XLSX.utils.sheet_to_json(sheet);
-          return initial;
-        }, {});
+  onFileChange(ev) {
+    let workBook = null;
+    let jsonData = null;
+    const reader = new FileReader();
+    const file = ev.target.files[0];
+    reader.onload = (event) => {
+      const data = reader.result;
+      workBook = XLSX.read(data, { type: 'binary' });
+      jsonData = workBook.SheetNames.reduce((initial, name) => {
+        const sheet = workBook.Sheets[name];
+        initial[name] = XLSX.utils.sheet_to_json(sheet);
+        return initial;
+      }, {});
 
-       this.exportedTeachers = [];
-        const d = jsonData;
-        for (let i = 0; i < d.exel.length; i++) {
-            const la_ligne = d.exel[i];
-            const element: any = {};
-            element.teacherTypeId = this.teacherTypeId,
-              element.lastName = la_ligne.nom;
-              element.firstName = la_ligne.prenoms,
-              element.phoneNumber = la_ligne.contact1,
-              element.secondPhoneNumber = la_ligne.contact2,
-              element.email = la_ligne.email;
-              element.courses = [];
-              if (la_ligne.cours1) {
-                element.courses = [ ...element.courses, la_ligne.cours1];
-              }if (la_ligne.cours2) {
-                element.courses = [ ...element.courses, la_ligne.cours2];
-              }if (la_ligne.cours3) {
-                element.courses = [ ...element.courses, la_ligne.cours3];
-              }if (la_ligne.cours4) {
-                element.courses = [ ...element.courses, la_ligne.cours4];
-              }if (la_ligne.cours5) {
-                element.courses = [ ...element.courses, la_ligne.cours5];
-              }if (la_ligne.cours6) {
-                element.courses = [ ...element.courses, la_ligne.cours6];
-              }if (la_ligne.cours7) {
-                element.courses = [ ...element.courses, la_ligne.cours7];
-              }if (la_ligne.cours8) {
-                element.courses = [ ...element.courses, la_ligne.cours8];
-              }if (la_ligne.cours9) {
-                element.courses = [ ...element.courses, la_ligne.cours9];
-              }
-              if (la_ligne.cours10) {
-                element.courses = [ ...element.courses, la_ligne.cours10];
-              }
-            this.exportedTeachers = [...this.exportedTeachers, element];
+      this.exportedTeachers = [];
+      const d = jsonData;
+      for (let i = 0; i < d.exel.length; i++) {
+        const la_ligne = d.exel[i];
+        const element: any = {};
+        element.teacherTypeId = this.teacherTypeId,
+          element.lastName = la_ligne.nom;
+        element.firstName = la_ligne.prenoms,
+          element.phoneNumber = la_ligne.contact1,
+          element.secondPhoneNumber = la_ligne.contact2,
+          element.email = la_ligne.email;
+        element.courses = [];
+        if (la_ligne.cours1) {
+          element.courses = [...element.courses, la_ligne.cours1];
+        } if (la_ligne.cours2) {
+          element.courses = [...element.courses, la_ligne.cours2];
+        } if (la_ligne.cours3) {
+          element.courses = [...element.courses, la_ligne.cours3];
+        } if (la_ligne.cours4) {
+          element.courses = [...element.courses, la_ligne.cours4];
+        } if (la_ligne.cours5) {
+          element.courses = [...element.courses, la_ligne.cours5];
+        } if (la_ligne.cours6) {
+          element.courses = [...element.courses, la_ligne.cours6];
+        } if (la_ligne.cours7) {
+          element.courses = [...element.courses, la_ligne.cours7];
+        } if (la_ligne.cours8) {
+          element.courses = [...element.courses, la_ligne.cours8];
+        } if (la_ligne.cours9) {
+          element.courses = [...element.courses, la_ligne.cours9];
         }
-        this.show = 'export';
+        if (la_ligne.cours10) {
+          element.courses = [...element.courses, la_ligne.cours10];
+        }
+        this.exportedTeachers = [...this.exportedTeachers, element];
+      }
+      this.show = 'export';
 
-        // this.setDownload(dataString);
-      };
-      reader.readAsBinaryString(file);
-    }
+      // this.setDownload(dataString);
+    };
+    reader.readAsBinaryString(file);
+  }
 
 
-    setDownload(data) {
-     // this.willDownload = true;
-      setTimeout(() => {
-        const el = document.querySelector('#download');
-        el.setAttribute('href', `data:text/json;charset=utf-8,${encodeURIComponent(data)}`);
-        el.setAttribute('download', 'xlsxtojson.json');
-      }, 1000);
-    }
+  setDownload(data) {
+    // this.willDownload = true;
+    setTimeout(() => {
+      const el = document.querySelector('#download');
+      el.setAttribute('href', `data:text/json;charset=utf-8,${encodeURIComponent(data)}`);
+      el.setAttribute('download', 'xlsxtojson.json');
+    }, 1000);
+  }
 }
