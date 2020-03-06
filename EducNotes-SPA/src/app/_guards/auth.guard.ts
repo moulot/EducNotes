2c52e76e6fca36edaf441f7a72f6cf70802e875b
MohKabore <mhkabore@gmail.com>
2020-03-06 21:49:32 +0000

    modification du login pour garder le request url

--- EducNotes-SPA/src/app/_guards/auth.guard.ts
+++ EducNotes-SPA/src/app/_guards/auth.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router, ActivatedRouteSnapshot } from '@angular/router';
+import { CanActivate, Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
@@ -9,26 +9,32 @@ import { AlertifyService } from '../_services/alertify.service';
 })
 export class AuthGuard implements CanActivate {
   constructor(private authService: AuthService, private router: Router,
-    private alertify: AlertifyService) {}
+    private alertify: AlertifyService) { }
 
-  canActivate(next: ActivatedRouteSnapshot): boolean {
-    const roles = next.firstChild.data['roles'] as Array<string>;
-    if (roles) {
-      const match = this.authService.roleMatch(roles);
-      if (match) {
-        return true;
-      } else {
-        this.router.navigate(['home']);
-        this.alertify.error('You are not authorised to access this area');
-      }
-    }
-
+  canActivate(next: ActivatedRouteSnapshot,  state: RouterStateSnapshot): boolean {
     if (this.authService.loggedIn()) {
-      return true;
-    }
+      const roles = next.firstChild.data['roles'] as Array<string>;
+      if (roles) {
+        const match = this.authService.roleMatch(roles);
+        if (match) {
+          return true;
+        } else {
+          this.router.navigate(['home']);
+          this.alertify.error('You are not authorised to access this area');
+        }
+      }
 
-    // this.alertify.error('veuillez vous connecter...');
+      if (this.authService.loggedIn()) {
+        return true;
+      }
+
+      // this.alertify.error('veuillez vous connecter...');
+      this.router.navigate(['/signIn']);
+      return false;
+    }
+    this.authService.redirectUrl = state.url;
     this.router.navigate(['/signIn']);
     return false;
+
   }
 }
